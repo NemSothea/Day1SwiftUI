3482f950a651732dbd9d251e11dd2f693c077b57
nemsothea <nemsothea13@gmail.com>
2021-01-20 08:14:26 +0700

    Initial Commit

---
+++ Day1/Day1App.swift
@@ -0,0 +1,17 @@
+//
+//  Day1App.swift
+//  Day1
+//
+//  Created by SotheaMac on 20/1/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Day1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
